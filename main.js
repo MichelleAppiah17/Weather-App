ef59824e9db97559675efb7fd9e2bce0efb00adc
Michelle Appiah <michappiah67@gmail.com>
2023-09-09 22:26:27 +0000

    error message

--- main.js
+++ main.js
@@ -6,47 +6,60 @@ const searchBtn = document.querySelector(".searchBar button");
 const weather_icon = document.querySelector(".weatherIcon");
 
 async function checkWeather(location) {
-  const response = await fetch(`${apiUrl}${location}&key=${apiKey}`);
-  const data = await response.json();
 
-  weather_icon.src = data.current.condition.icon;
-  document.querySelector(".location").innerHTML = data.location.name;
-  document.querySelector(".temp").innerHTML = Math.round(data.current.temp_c) + "°";
-  document.querySelector(".humidity").innerHTML = data.current.humidity + "%";
-  document.querySelector(".wind").innerHTML = data.current.wind_kph + "km/h";
-  document.querySelector(".windDirection").innerHTML = data.current.wind_dir;
+    try{
+        const response = await fetch(`${apiUrl}${location}&key=${apiKey}`);
 
-  const todayTime = document.querySelectorAll(".todayTime");
-  const todayIcon = document.querySelectorAll(".todayIcon");
-  const todayTemp = document.querySelectorAll(".todayTemp");
+        if(response.ok){
+          
+        const data = await response.json();
+      
+        weather_icon.src = data.current.condition.icon;
+        document.querySelector(".location").innerHTML = data.location.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.current.temp_c) + "°";
+        document.querySelector(".humidity").innerHTML = data.current.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.current.wind_kph + "km/h";
+        document.querySelector(".windDirection").innerHTML = data.current.wind_dir;
+      
+        const todayTime = document.querySelectorAll(".todayTime");
+        const todayIcon = document.querySelectorAll(".todayIcon");
+        const todayTemp = document.querySelectorAll(".todayTemp");
+      
+        console.log(data)
+      
+        const hourForecast = data.forecast.forecastday[0]?.hour;
+      
+        const currentTime = new Date();
+        let currentHour = currentTime.getHours();
+      
+        const timeFormat = { hour:"numeric", minute:"2-digit" };
+      
+         
+        for (let i = 0; i < todayTime.length; i++) {
+          if (currentHour >= hourForecast.length) {
+            currentHour = currentHour % hourForecast.length;
+          }
+      
+          const rawTime = hourForecast[currentHour].time;
+          const formattedTime = new Date(rawTime).toLocaleTimeString("en-US", timeFormat);
+           
+      
+          todayTime[i].innerHTML = formattedTime;
+          todayTemp[i].innerHTML = hourForecast[currentHour].temp_c + "°";
+          todayIcon[i].src = hourForecast[currentHour].condition.icon;
+          currentHour += 3; 
+        }
 
-  console.log(data)
+        document.querySelector(".weather").style.display = "block";
+      }else {
+      document.querySelector(".weather").style.display = "none";
+      alert("Invalid Location Name");
+     }
 
-  const hourForecast = data.forecast.forecastday[0]?.hour;
-
-  const currentTime = new Date();
-  let currentHour = currentTime.getHours();
-
-  const timeFormat = { hour:"numeric", minute:"2-digit" };
-
-   
-  for (let i = 0; i < todayTime.length; i++) {
-    if (currentHour >= hourForecast.length) {
-      currentHour = currentHour % hourForecast.length;
+    }catch{
+    console.log('error');
     }
-
-    const rawTime = hourForecast[currentHour].time;
-    const formattedTime = new Date(rawTime).toLocaleTimeString("en-US", timeFormat);
-     
-
-    todayTime[i].innerHTML = formattedTime;
-    todayTemp[i].innerHTML = hourForecast[currentHour].temp_c + "°";
-    todayIcon[i].src = hourForecast[currentHour].condition.icon;
-    currentHour += 3; 
-  }
-
-  document.querySelector(".weather").style.display = "block";
-}
+}   
 
 searchBtn.addEventListener("click", async () => {
   const location = searchBox.value;
